18aa6d3392357136f10c48c4f303fd38df349da4
shakhawat1355 <hossain.shakhawat@brainstation-23.com>
2023-08-05 01:05:01 +0600

    Add project files.

---
+++ DemoWorker/Program.cs
@@ -0,0 +1,10 @@
+using DemoWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
